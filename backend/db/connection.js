e46ead3d44a4878a947f1f432906efd6499e878d
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-08-12 00:13:05 +0530

    complete

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,7 +1,7 @@
 let mongoose=require('mongoose');
 
-SECRET_KEY='y32y2bcc83vvchcyklacvznutrkjBcggvcvDCCd'
-MONGO_URI='mongodb+srv://ankursasmal2024:Ankur123@cluster0.poymr.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0'
+// SECRET_KEY='y32y2bcc83vvchcyklacvznutrkjBcggvcvDCCd'
+// MONGO_URI='mongodb+srv://ankursasmal2024:Ankur123@cluster0.poymr.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0'
 
 let conection= mongoose.connect('mongodb+srv://ankursasmal2024:Ankur123@cluster0.poymr.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log('db connect');
